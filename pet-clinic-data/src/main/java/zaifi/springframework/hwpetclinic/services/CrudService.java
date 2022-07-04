3364f6468bd7639787cd18599d9d3f348586aa2f
I2C\mwaseem01 <mwaseem01@i2cinc.com>
2022-07-04 16:19:20 +0500

    Refactoring to Common Crud Interface. Closes #11

---
+++ pet-clinic-data/src/main/java/zaifi/springframework/hwpetclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package zaifi.springframework.hwpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(ID t);
+
+    void deleteById(ID id);
+}
--- pet-clinic-data/src/main/java/zaifi/springframework/hwpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/zaifi/springframework/hwpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package zaifi.springframework.hwpetclinic.services;
 
 import zaifi.springframework.hwpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
