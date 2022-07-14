ed8beaf9625278fde26fb9d6bde9c654b8390515
HUZAIFA <huzaifawaseem97@gmail.com>
2022-07-15 00:43:40 +0500

    Added Vets List and Updated Controller and Vets Index Page. Closes #15

--- pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/controller/VetController.java
@@ -1,12 +1,22 @@
 package zaifi.springframework.hwpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zaifi.springframework.hwpetclinic.services.VetService;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listOfVets(){
+    public String listOfVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
