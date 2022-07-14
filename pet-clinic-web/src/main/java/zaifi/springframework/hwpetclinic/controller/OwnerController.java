9ad1811bea7000e9719cb079368a0e660d2d4841
HUZAIFA <huzaifawaseem97@gmail.com>
2022-07-15 00:33:40 +0500

    Added Owners List and Updated Controller and Owners Index Page. Closes #16

--- pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/controller/OwnerController.java
@@ -1,13 +1,23 @@
 package zaifi.springframework.hwpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zaifi.springframework.hwpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOfOwners(){
+    public String listOfOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
