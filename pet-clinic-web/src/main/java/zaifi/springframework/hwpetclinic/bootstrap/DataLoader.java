e72ce1efafcf2ba0d9eeaddacbcb4d4a068ce448
HUZAIFA <huzaifawaseem97@gmail.com>
2022-07-17 00:15:17 +0500

    Added PetTypes to Boostrap. Closes #32

--- pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/zaifi/springframework/hwpetclinic/bootstrap/DataLoader.java
@@ -3,22 +3,35 @@ package zaifi.springframework.hwpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import zaifi.springframework.hwpetclinic.model.Owner;
+import zaifi.springframework.hwpetclinic.model.PetType;
 import zaifi.springframework.hwpetclinic.model.Vet;
 import zaifi.springframework.hwpetclinic.services.OwnerService;
+import zaifi.springframework.hwpetclinic.services.PetTypeService;
 import zaifi.springframework.hwpetclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setId(1L);
         owner1.setFirstName("Robert");
